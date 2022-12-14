dbc32bf4bd2a92cb8f92843584ca2bb8b93d3655
Jun_Q <junlee36520620@gmail.com>
2022-12-14 23:51:15 +0900

    try catch

--- src/bind/PropsScreen.js
+++ src/bind/PropsScreen.js
@@ -1,5 +1,12 @@
-const PropsScreen = (props) => {
+const PropsScreen = ( props ) => {
     const check = () => {
+        let result;
+        try {
+            result = props.children;
+        } catch ( err ) {
+            console.log( err );
+        }
+
         if ( props.children ) {
             console.log( 'props.children :: ', props.children );
         }
