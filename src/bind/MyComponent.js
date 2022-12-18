adba51b6db2cf6d75dfdefe049d4de76103768bc
Jun_Q <junlee36520620@gmail.com>
2022-12-18 22:47:09 +0900

    counter

--- src/bind/MyComponent.js
+++ src/bind/MyComponent.js
@@ -1,16 +1,29 @@
-const MyComponent = ( props ) => {
-    const {name, children} = props;
+// const MyComponent = ( props ) => {
+//     const {name, children} = props;
+
+import PropTypes from "prop-types";
+
+
+// props 검증하는 방법
+const MyComponent = ( {name, favoritNumber, children} ) => {
 
     return (
         <div>
             My name is {name}!! <br/>
             children value is {children} !!
+            <br/>
+            My favorite number is {favoritNumber} !!
         </div>
     )
 }
 
 MyComponent.defaultProps = {
     name: 'Deafult Name'
+}
+
+MyComponent.defaultTypes = {
+    name          : PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired // 필수로 전달 받아야함을 의미한다.
 }
 
 
--- src/useState/Counter.js
+++ src/useState/Counter.js
@@ -1,17 +1,42 @@
-import {useState} from "react";
+import {Component, useState} from "react";
 
-const Counter = () => {
-    const [value, setValue] = useState( 0 );
 
-    return (
-        <div>
-            <p>
-                현재 카운터 값은 <b>{value}</b>.
-            </p>
-            <button onClick={() => setValue( value + 1 )}> + 1</button>
-            <button onClick={() => setValue( value - 1 )}> - 1</button>
-        </div>
-    )
+class Counter extends Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            number: 0
+        };
+    }
+
+    render() {
+        const {number} = this.state;
+        return (
+            <>
+                <h1>{number}</h1>
+                <button onClick={() => {
+                    this.setState( {number: number + 1} );
+                }}>
+                    +1
+                </button>
+            </>
+        );
+    }
 }
 
+// const Counter = () => {
+//     const [value, setValue] = useState( 0 );
+//
+//     return (
+//         <div>
+//             <p>
+//                 현재 카운터 값은 <b>{value}</b>.
+//             </p>
+//             <button onClick={() => setValue( value + 1 )}> + 1</button>
+//             <button onClick={() => setValue( value - 1 )}> - 1</button>
+//         </div>
+//     )
+// }
+//
 export default Counter;
