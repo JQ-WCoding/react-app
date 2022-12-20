ad862e933e565aa717ac966b59a10b8e0acf050c
Jun_Q <junlee36520620@gmail.com>
2022-12-20 22:53:45 +0900

    state and constructor

--- src/event/EventPractice.js
+++ src/event/EventPractice.js
@@ -1,6 +1,31 @@
 import {Component} from "react";
 
 class EventPractice extends Component {
+    state = {
+        username: '',
+        message : ''
+    }
+
+    constructor( props ) {
+        super( props );
+        this.handleChange = this.handleChange.bind( this );
+        this.handleClick = this.handleClick.bind( this );
+    }
+
+    handleChange( e ) {
+        this.setState( {
+            [e.target.name]: e.target.value
+        } );
+    }
+
+    handleClick() {
+        alert( this.state.username + ': ' + this.state.message );
+        this.setState( {
+            username: '',
+            message : ''
+        } );
+    }
+
     render() {
         return (
             <>
@@ -8,10 +33,19 @@ class EventPractice extends Component {
                 <input type="text"
                        name="message"
                        placeholder="anything"
-                       onChange={( e ) => {
-                           console.log( e );
-                       }}
+                       value={this.state.message}
+                    // onChange={( e ) => this.setState( {
+                    //     message: e.target.value
+                    // } )}
+                       onChange={this.handleChange}
                 />
+                <input type="text"
+                       name="message"
+                       placeholder="anything"
+                       value={this.state.message}
+                       onChange={this.handleChange}
+                />
+                <button onClick={this.handleClick}>Confirm</button>
             </>
         );
     }
