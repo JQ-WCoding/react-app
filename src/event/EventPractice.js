0426e69fa1a23ab683f8d6e32689c920004c99ce
Jun_Q <junlee36520620@gmail.com>
2022-12-21 20:51:21 +0900

    함수형으로 변환

--- src/event/EventPractice.js
+++ src/event/EventPractice.js
@@ -1,54 +1,97 @@
-import {Component} from "react";
+// import {Component} from "react";
+//
+// class EventPractice extends Component {
+//     state = {
+//         username: '',
+//         message : ''
+//     }
+//
+//     constructor( props ) {
+//         super( props );
+//         this.handleChange = this.handleChange.bind( this );
+//         this.handleClick = this.handleClick.bind( this );
+//     }
+//
+//     handleChange( e ) {
+//         this.setState( {
+//             [e.target.name]: e.target.value
+//         } );
+//     }
+//
+//     handleClick() {
+//         alert( this.state.username + ': ' + this.state.message );
+//         this.setState( {
+//             username: '',
+//             message : ''
+//         } );
+//     }
+//
+//     render() {
+//         return (
+//             <>
+//                 <h1>Event Practice</h1>
+//                 <input type="text"
+//                        name="message"
+//                        placeholder="anything"
+//                        value={this.state.message}
+//                     // onChange={( e ) => this.setState( {
+//                     //     message: e.target.value
+//                     // } )}
+//                        onChange={this.handleChange}
+//                 />
+//                 <input type="text"
+//                        name="message"
+//                        placeholder="anything"
+//                        value={this.state.message}
+//                        onChange={this.handleChange}
+//                 />
+//                 <button onClick={this.handleClick}>Confirm</button>
+//             </>
+//         );
+//     }
+// }
+//
+// export default EventPractice;
 
-class EventPractice extends Component {
-    state = {
-        username: '',
-        message : ''
+
+// 동일하게 함수로 구현
+import {useState} from "react";
+
+const EventPractice = () => {
+    const [username, setUsername] = useState( '' );
+    const [message, setMessage] = useState( '' );
+
+    const onChangeUsername = e => setUsername( e.target.value );
+    const onChangeMessage = e => setMessage( e.target.value );
+
+    const onClick = () => {
+        alert( username + ': ' + message );
+        setUsername( '' );
+        setMessage( '' );
     }
 
-    constructor( props ) {
-        super( props );
-        this.handleChange = this.handleChange.bind( this );
-        this.handleClick = this.handleClick.bind( this );
+    const onKeyPress = e => {
+        if ( e.key === 'Enter' ) {
+            onClick();
+        }
     }
 
-    handleChange( e ) {
-        this.setState( {
-            [e.target.name]: e.target.value
-        } );
-    }
+    return (
+        <>
+            <h1>Event Practice</h1>
+            <input type="text"
+                   name="username"
+                   placeholder="User Name"
+                   value={username}
+                   onChange={onChangeUsername}/>
+            <input type="text"
+                   name="messgae"
+                   placeholder="Input Anything that you want"
+                   value={message}
+                   onChange={onChangeMessage}
+                   onKeyPress={onKeyPress}/>
 
-    handleClick() {
-        alert( this.state.username + ': ' + this.state.message );
-        this.setState( {
-            username: '',
-            message : ''
-        } );
-    }
-
-    render() {
-        return (
-            <>
-                <h1>Event Practice</h1>
-                <input type="text"
-                       name="message"
-                       placeholder="anything"
-                       value={this.state.message}
-                    // onChange={( e ) => this.setState( {
-                    //     message: e.target.value
-                    // } )}
-                       onChange={this.handleChange}
-                />
-                <input type="text"
-                       name="message"
-                       placeholder="anything"
-                       value={this.state.message}
-                       onChange={this.handleChange}
-                />
-                <button onClick={this.handleClick}>Confirm</button>
-            </>
-        );
-    }
+            <button onClick={onClick}>Confirm</button>
+        </>
+    );
 }
-
-export default EventPractice;
